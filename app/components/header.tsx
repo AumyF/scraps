295133c893a86ae7a667afddb2e5fdaa6b0cbdaa
Aumy <aumy@outlook.jp>
2022-01-08 11:20:48 +0900

    add padding to container

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,8 +2,8 @@ import { VFC } from "react";
 import { Link } from "remix";
 
 export const Header: VFC = () => (
-  <header className="flex border-b border-gray-200 py-2">
-    <div className="flex justify-between w-full max-w-6xl mx-auto">
+  <header className="flex border-b border-gray-200">
+    <div className="flex justify-between w-full max-w-6xl mx-auto p-4">
       <a href="/">
         <h1 className="text-3xl font-bold">Scrap</h1>
       </a>
--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -4,6 +4,6 @@ import { Header } from "./header";
 export const Layout: VFC<PropsWithChildren<{}>> = ({ children }) => (
   <div>
     <Header />
-    <main className="max-w-6xl mx-auto">{children}</main>
+    <main className="max-w-6xl mx-auto p-4">{children}</main>
   </div>
 );
