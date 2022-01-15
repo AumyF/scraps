2a2fa86d1813309c55edea1c5c8bfa3fc0b9c6ac
Aumy <aumy@outlook.jp>
2022-01-15 13:58:25 +0900

    show login status in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,29 @@
 import { VFC } from "react";
 import { Link } from "remix";
 
-export const Header: VFC = () => (
-  <header className="flex border-b border-gray-200">
-    <div className="flex justify-between p-4 mx-auto w-full max-w-6xl">
-      <a href="/">
-        <h1 className="text-3xl font-bold">Scrap</h1>
-      </a>
-      <Link to="/scraps/new" className="button brand">
-        New Scrap
-      </Link>
-    </div>
-  </header>
-);
+import { useSupabase } from "~/utils/supabase-client";
+
+export const Header: VFC = () => {
+  const supabase = useSupabase();
+  const user = supabase.auth.user();
+
+  const signedIn = user !== null;
+  return (
+    <header className="flex border-b border-gray-200">
+      <div className="flex justify-between p-4 mx-auto w-full max-w-6xl">
+        <a href="/">
+          <h1 className="text-3xl font-bold">Scrap</h1>
+        </a>
+        {signedIn ? (
+          <Link to="/scraps/new" className="button brand">
+            New Scrap
+          </Link>
+        ) : (
+          <Link to="/auth" className="button brand">
+            Sign in
+          </Link>
+        )}
+      </div>
+    </header>
+  );
+};
